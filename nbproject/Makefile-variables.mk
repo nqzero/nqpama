5256407870d0ab13f446ad637532213c4c7fd85d
lytles <seth_lytle@yahoo.com>
2010-12-12 20:21:58 -0500

    ... looks like everything except private in nbproject is needed (eg, to reconfigure the project)

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,16 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Default configuration
+CND_PLATFORM_Default=GNU-Linux-x86
+CND_ARTIFACT_DIR_Default=src
+CND_ARTIFACT_NAME_Default=pulseaudio-mixer-applet
+CND_ARTIFACT_PATH_Default=src/pulseaudio-mixer-applet
+CND_PACKAGE_DIR_Default=dist/Default/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Default=pama.tar
+CND_PACKAGE_PATH_Default=dist/Default/GNU-Linux-x86/package/pama.tar
